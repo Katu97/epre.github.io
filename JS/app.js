440b236cf53ce485585dd19d9db89e44591085d1
Katu97 <skatuchin.97.sk@gmail.com>
2026-02-27 12:40:19 -0300

    varios cambios

--- JS/app.js
+++ JS/app.js
@@ -5,26 +5,51 @@ function vaciar(text1, text2){
 
 function login(){
     let userAdmin = ["admin", "admin123"]
-    let userAtUsuario = ["atusuario", "atusuario123"]
+    let userAtUsuario = ["atuser", "atuser123"]
     let user = document.getElementById('user');
     let pass = document.getElementById('pass');
+    let error = document.getElementById("error");
     
     //ADMIN
     if (user.value === userAdmin[0] && pass.value === userAdmin[1]){
         //console.log("entro como admin");
+        error.textContent = "";
         vaciar(user, pass);
-        window.location.href = "sistemaRegistro.html";
-
+        alert("Bienvenido, usted ha ingresado como Admnistrador");
+        window.location.href = "sistemaRegistro.html?rol=admin";
+    
     //EMPLEADO
     } else if (user.value === userAtUsuario[0] && pass.value === userAtUsuario[1]){
-        console.log("entro como empleado");
+        //console.log("entro como empleado");
         vaciar(user, pass);
-        //window.location.href = "sistemaExpedientes.html";
+        error.textContent = "";
+        alert("Bienvenido, usted ha ingresado como usuario");
+        window.location.href = "registroReclamos.html?rol=usuario";
     
     //MAL LA CONTRASEÑA
     } else {
-        alert("Credenciales de ingreso incorrectas, intentelo de nuevo por favor");
+        error.textContent = "Credenciales incorrectas. Intente nuevamente.";
         vaciar(user, pass);
     }
     
+}
+
+function mensajeBienvenida() {
+    let params = new URLSearchParams(window.location.search);
+    let rol = params.get("rol");
+    let userms = document.getElementById("user-ms");
+
+    if (rol === "admin") {
+        userms.textContent = "Bienvenido, usted ha ingresado como Administrador";
+    } else if (rol === "usuario") {
+        userms.textContent = "Bienvenido, usted ha ingresado como usuario";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", mensajeBienvenida);
+
+function verificarSalida(){
+    if(confirm("¿Está seguro que desea continuar?")){
+        window.location.href = "index.html";
+    }
 }
